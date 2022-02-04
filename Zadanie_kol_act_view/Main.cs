06bd0dc06a80165be39ec7e921741468db193b70
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2022-02-04 16:45:35 +0300

    Add files via upload

---
+++ Zadanie_kol_act_view/Main.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_kol_act_view
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            List<FamilyInstance> finstances = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                .OfCategory(BuiltInCategory.OST_Columns)
+                .WhereElementIsNotElementType()
+                .Cast<FamilyInstance>()
+                .ToList();
+            TaskDialog.Show("Columns count", $"Количество колонн на виде {finstances.Count.ToString()}");
+            return Result.Succeeded;
+        }
+    }
+}
